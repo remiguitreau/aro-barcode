91d950230518da3805609e4a9a337eb859e30159
Rémi Guitreau <remi.guitreau@free.fr>
2014-11-20 21:05:16 +0100

    Import d'une ligne par equipement

--- src/main/java/fr/remiguitreau/aroeven/barcode/xls/XLSEquipmentsImporter.java
+++ src/main/java/fr/remiguitreau/aroeven/barcode/xls/XLSEquipmentsImporter.java
@@ -35,47 +35,29 @@ public class XLSEquipmentsImporter {
         final ImportResult result = new ImportResult();
         log.info("Import equipments from line {} to {}", equipmentSheet.getFirstRowNum() + 2,
                 equipmentSheet.getLastRowNum() + 1);
+        final List<AroEquipment> equipments = new ArrayList<>();
         for (int i = equipmentSheet.getFirstRowNum() + 1; i <= equipmentSheet.getLastRowNum(); i++) {
             try {
-                for (final AroEquipment equipment : extractEquipmentsFromRow(equipmentSheet.getRow(i))) {
-                    addEquipmentToSize(equipmentsBySize, equipment);
-                }
+                equipments.addAll(extractEquipmentsFromRow(equipmentSheet.getRow(i)));
             } catch (final Exception ex) {
                 log.warn("Error on line " + i, ex);
                 result.getErrors().put(Integer.valueOf(i), ex.getMessage());
-            }
-        }
-        final List<AroEquipment> equipments = new ArrayList<>();
-        for (final List<AroEquipment> list : equipmentsBySize.values()) {
-            for (final AroEquipment equipment : list) {
-                equipments.add(equipment);
             }
         }
         result.setEquipments(equipments);
         return result;
     }
 
-    private void addEquipmentToSize(final Map<String, List<AroEquipment>> equipmentsBySize,
-            final AroEquipment equipment) {
-        if (!equipmentsBySize.containsKey(equipment.getSize())) {
-            equipmentsBySize.put(equipment.getSize(), new ArrayList<AroEquipment>());
-        }
-        equipment.setNumber(String.valueOf(equipmentsBySize.get(equipment.getSize()).size() + 1));
-        equipmentsBySize.get(equipment.getSize()).add(equipment);
-    }
-
     private List<AroEquipment> extractEquipmentsFromRow(final XSSFRow row) {
         final List<AroEquipment> equipments = new ArrayList<AroEquipment>();
         try {
-            final int numberOfEquipments = AroXLSColumns.NUMBER.extractValueFromRow(row);
+            final int number = AroXLSColumns.NUMBER.extractValueFromRow(row);
             final String brand = AroXLSColumns.BRAND.extractValueFromRow(row);
             final String size = AroXLSColumns.SIZE.extractValueFromRow(row);
             final boolean pair = AroXLSColumns.PAIR.extractValueFromRow(row);
-            log.info("Extract {} equipments with brand '{}', size={} and pair={}", numberOfEquipments, brand,
-                    size, pair);
-            for (int i = 0; i < numberOfEquipments; i++) {
-                equipments.add(new AroEquipment(brand, size, String.valueOf(i + 1), pair));
-            }
+            log.info("Extract new equipment '{}' with brand '{}', size={} and pair={}", number, brand, size,
+                    pair);
+            equipments.add(new AroEquipment(brand, size, String.valueOf(number), pair));
         } catch (final AroEquipmentImportException ex) {
             throw ex;
         } catch (final Exception ex) {
--- src/test/java/fr/remiguitreau/aroeven/barcode/xls/XLSEquipmentsImporterTest.java
+++ src/test/java/fr/remiguitreau/aroeven/barcode/xls/XLSEquipmentsImporterTest.java
@@ -18,25 +18,11 @@ public class XLSEquipmentsImporterTest {
     public void test_equipment_import() {
         final ImportResult result = importer.importEquipmentsFromInputStream(XLSEquipmentsImporterTest.class.getResourceAsStream("equipments_ok.xlsx"));
 
-        Assert.assertEquals(18, result.getEquipments().size());
+        Assert.assertEquals(4, result.getEquipments().size());
 
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Salomon", "110", "1", true)));
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Salomon", "110", "2", true)));
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Salomon", "110", "3", true)));
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Salomon", "110", "4", true)));
         Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Salomon", "110", "5", true)));
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Head", "110", "6", true)));
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Head", "110", "7", true)));
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Head", "110", "8", true)));
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Head", "110", "9", true)));
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Salomon", "120", "1", true)));
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Salomon", "120", "2", true)));
+        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Head", "110", "4", true)));
         Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Salomon", "120", "3", true)));
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Helmet", "XL", "1", false)));
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Helmet", "XL", "2", false)));
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Helmet", "XL", "3", false)));
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Helmet", "XL", "4", false)));
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Helmet", "XL", "5", false)));
         Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Helmet", "XL", "6", false)));
         Assert.assertTrue(result.getErrors().isEmpty());
     }
@@ -45,9 +31,7 @@ public class XLSEquipmentsImporterTest {
     public void test_equipment_import_with_some_bad_lines() {
         final ImportResult result = importer.importEquipmentsFromInputStream(XLSEquipmentsImporterTest.class.getResourceAsStream("equipments_bad.xlsx"));
 
-        Assert.assertEquals(3, result.getEquipments().size());
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Salomon", "120", "1", true)));
-        Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Salomon", "120", "2", true)));
+        Assert.assertEquals(1, result.getEquipments().size());
         Assert.assertTrue(result.getEquipments().contains(new AroEquipment("Salomon", "120", "3", true)));
         Assert.assertEquals(3, result.getErrors().size());
     }
